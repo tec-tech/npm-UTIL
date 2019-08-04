156457d52dce4bc6da54a7ef64c1df4c0d5dba6f
yoshihir0 <yoshi@tectech.jp>
2019-08-04 23:31:29 +0900

    画像リサイズ機能追加

--- Util.js
+++ Util.js
@@ -56,18 +56,75 @@ const Util = {
 		return target;
 	},
 
-	//===============================================
-	// ランダム文字列を生成
-	//===============================================
-	random: (size)=>{
-		if(!size) size = 8;
-		var c = "abcdefghijklmnopqrstuvwxyz0123456789";
-		var cl = c.length;
-		var r = "";
-		for(var i=0; i<size; i++){
-			r += c[Math.floor(Math.random()*cl)];
+	//===================================
+	// 画像（file）をリサイズ
+	//===================================
+	ImgResize: function(file, maxSize, callback) {
+		const fr = new FileReader();
+		// base64エンコード
+		fr.onload=(e)=>{
+			// 画像（BASE64）をリサイズ
+			Util.ImgB64Resize(e.target.result, maxSize, (base64)=>{
+				// base64の文字列をファイルオブジェクトに変換
+				callback(Util.Base64toFile(base64, file.name));
+			});
 		}
-		return r;
+		fr.readAsDataURL(file);
+	},
+
+	//===============================================
+	// 画像（BASE64）をリサイズ
+	//===============================================
+	ImgB64Resize: function(imgB64, maxSize, callback) {
+		var img_type = imgB64.substring(5, imgB64.indexOf(";"));
+		var image = new Image();
+		image.onload = function() {
+			var width, height;
+			// 最大サイズより小さい時は何もしない
+			if(maxSize > image.width && maxSize > image.height){
+				callback(imgB64);
+				return;
+			}else{
+				// 横長の画像は横のサイズを指定値にあわせる
+				if(image.width > image.height){
+					var ratio = image.height/image.width;
+					width = maxSize;
+					height = Math.floor(maxSize * ratio);
+				// 縦長の画像は縦のサイズを指定値にあわせる
+				} else {
+					var ratio = image.width/image.height;
+					width = Math.floor(maxSize * ratio);
+					height = maxSize;
+				}
+			}
+			// New Canvas
+			var canvas = document.createElement('canvas');
+			canvas.width = width;
+			canvas.height = height;
+			// Draw (Resize)
+			var ctx = canvas.getContext('2d');
+			ctx.drawImage(image, 0, 0, width, height);
+			// Destination Image
+			callback(canvas.toDataURL(img_type));
+		};
+		image.src = imgB64;
+	},
+
+	//===============================================
+	// base64の文字列をファイルオブジェクトに変換
+	//===============================================
+	Base64toFile: function(base64, name){
+		if(!name) name="file";
+		// base64のデコード
+		var bin = atob(base64.replace(/^.*,/, ''));
+		// バイナリデータ化
+		var buffer = new Uint8Array(bin.length);
+		for (var i = 0; i < bin.length; i++) {
+			buffer[i] = bin.charCodeAt(i);
+		}
+		// ファイルオブジェクト生成
+		var img_type = base64.substring(5, base64.indexOf(";"));
+		return new File([buffer.buffer], name, {type: img_type});
 	},
 
 }
